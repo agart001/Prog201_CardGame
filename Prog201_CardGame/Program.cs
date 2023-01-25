296962055d543e5d782288faeede408266208f83
agart001 <agart.art@gmail.com>
2023-01-25 12:16:14 -0600

    Add project files.

---
+++ Prog201_CardGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Prog201_CardGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
