070973e35c6f55aaf53f577905876aae78676875
agart001 <agart.art@gmail.com>
2023-02-19 23:00:02 -0600

    Highest Match cont

--- Prog201_CardGame/HiM.cs
+++ Prog201_CardGame/HiM.cs
@@ -9,10 +9,16 @@ namespace Prog201_CardGame
 {
     internal class HiM : Game
     {
+        int MaxPointValue = 40;
+
+        int PlayerTotal = 0;
+        int DealerTotal = 0;
         int Round = 0;
+
         public HiM(string _Name, string _Desc, string[] _Suites, int _NumDecks, double _HouseMoney, int _HandSize) : base(_Name, _Desc, _Suites, _NumDecks, _HouseMoney, _HandSize)
         {
         }
+
 
         public override void Loop()
         {
@@ -28,6 +34,86 @@ namespace Prog201_CardGame
             GameLoop();
         }
 
+        string FavoredSuite(List<Card> _Hand)
+        {
+            int i = 0;
+            int[] Weights = new int[4];
+            foreach(string Suite in Suites) 
+            {
+                foreach(Card _Card in _Hand) 
+                {
+                    if(Suite == _Card.Suite) 
+                    {
+                        Weights[i] += 1;
+                    }
+                }
+                i++;
+            }
+
+            int FavoredValue = Weights.Max();
+            int FavoredIndex = Array.IndexOf(Weights, FavoredValue);
+
+            return Suites[FavoredIndex];
+        }
+
+        int GetTotal(List<Card> _Hand)
+        {
+            int Total = 0;
+            string Suite = FavoredSuite(_Hand);
+            foreach(Card _Card in _Hand)
+            {
+                if(_Card.Suite == Suite)
+                {
+                    Total += 10;
+                }
+            }
+
+            return Total;
+        }
+
+        void GetTotals()
+        {
+            PlayerTotal = GetTotal(Player.Hand);
+            DealerTotal = GetTotal(Dealer.Hand);
+
+            Print("Total: " + PlayerTotal);
+            SpaceLine();
+            Print("D Total: " + DealerTotal);
+        }
+
+        void PlayerDraw()
+        {
+            if (Question("Would you like to remove a card? (y/n)", "y", "n"))
+            {
+                Print("Would you like to remove a card and draw another? (index number)");
+                int index = InputInt();
+                Player.Hand.Remove(Player.Hand[index - 1]);
+                Dealer.Deal(Player.Hand, 1);
+
+                Player.ShowHand();
+            }
+        }
+
+        void DealerDraw()
+        {
+            if(DealerTotal < MaxPointValue)
+            {
+                string Suite = FavoredSuite(Dealer.Hand);
+                foreach (Card _Card in Dealer.Hand)
+                {
+                    if (!_Card.Suite.Contains(Suite))
+                    {
+                        Dealer.Hand.Remove(_Card);
+                        Dealer.Deal(Dealer.Hand, 1);
+                        break;
+                    }
+                }
+            }
+        }
+
+
+
+
         void GameLoop()
         {
             if (Round == 10)
@@ -38,18 +124,13 @@ namespace Prog201_CardGame
             {
                 Round++;
 
-                if (Question("Would you like to remove a card? (y/n)", "y", "n"))
-                {
-                    Print("Would you like to remove a card and draw another? (index number)");
-                    int index = InputInt();
-                    Player.Hand.Remove(Player.Hand[index - 1]);
-                    Dealer.Deal(Player.Hand, 1);
-                    Player.ShowHand();
-                }
-                else
-                {
-                    GameLoop();
-                }
+                GetTotals();
+
+                PlayerDraw();
+
+                DealerDraw();
+
+                GameLoop();
             }
         }
     }
